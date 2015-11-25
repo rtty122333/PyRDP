382234ea6fb450a7cb6f736b9b39e27685696ead
rtty122333 <wangbo7799@gmail.com>
2015-11-25 11:57:16 +0800

    add ico into PE file

--- rdcSetup.py
+++ rdcSetup.py
@@ -17,7 +17,7 @@ setup(
       name = 'PyQt Demo',
       version = '1.0',
       windows = [
-      {'script':'rdcCtl.py','icon_resources':[(1,'rdc.ico')]}], 
+      {'script':'rdcCtl.py','icon_resources':[(0,'rdc.ico')]}], 
       zipfile = None,
       options = {'py2exe': py2exe_options}
       )
